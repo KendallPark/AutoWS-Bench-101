ad317bc6a1c4ab574f7bde0f68af1a15ec3485ac
Kendall Park <kendallparkdev@gmail.com>
2024-04-19 10:05:48 -0500

    feat: add support for mps and cpu

--- fwrench/applications/pipeline.py
+++ fwrench/applications/pipeline.py
@@ -52,7 +52,11 @@ def main(
         handlers=[LoggingHandler()],
     )
     logger = logging.getLogger(__name__)
-    device = torch.device("cuda")
+    device = 'cpu'
+    if  torch.cuda.is_available():
+        device = 'cuda'
+    elif torch.backends.mps.is_available():
+        device = 'mps'
 
     ################ LOAD DATASET #############################################
 
